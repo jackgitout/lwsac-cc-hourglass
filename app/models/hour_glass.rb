7860030063437601695e1233d76c18ba0ffc2fd4
Jack Zhang <junbojack@gmail.com>
2022-08-16 23:16:23 +0800

    main/added-test

--- app/models/hour_glass.rb
+++ app/models/hour_glass.rb
@@ -1,2 +1,5 @@
 class HourGlass < ApplicationRecord
+  def max_hour_glass_sum(arr)
+    # Write your code here
+  end
 end
--- spec/models/hour_glass_spec.rb
+++ spec/models/hour_glass_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe HourGlass, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:hour_glass_array) { HourGlass.create }
+
+  describe '#max_hour_glass_sum' do
+    context 'when finding the largest hourglass sum through first array' do
+      let(:array) do
+        [
+          [-9, -9, -9, 1, 1, 1],
+          [0, -9, 0, 4, 3, 2],
+          [-9, -9, -9, 1, 2, 3],
+          [0, 0, 8, 6, 6, 0],
+          [0, 0, 0, -2, 0, 0],
+          [0, 0, 1, 2, 4, 0]
+        ]
+      end
+
+      it 'should return 28' do
+        expect(subject.max_hour_glass_sum(array)).to eq(28)
+      end
+    end
+  end
 end
